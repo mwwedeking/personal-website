a8acb7f51d71414f053592a4c3d1e3d3d7f4e8bb
mwwedeking <111778304+mwwedeking@users.noreply.github.com>
2024-06-20 11:18:26 -0400

    Added parallax effect

--- functions.js
+++ functions.js
@@ -1,13 +1,4 @@
-const getResumeButtons = document.getElementsByClassName('button-header')
-console.log('getResumeButtons')
 
-// const button = getResumeButtons[0]
-// button.addEventListener('click', function(){
-//     console.log('clicked')
-// })
-// button.addEventListener('click', function(event){
-//     const buttonClicked = event.target
-// })
 
 
 
